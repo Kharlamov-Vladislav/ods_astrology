2bd2133cda425e6b9bbe93093b6625341c68aa83
Vladislav <48480239+Kharlamov-Vladislav@users.noreply.github.com>
2020-10-04 06:10:53 +0300

    fix regular expression in tokenaizer

--- scripts/science2tokens.py
+++ scripts/science2tokens.py
@@ -15,10 +15,10 @@ def from_texts_to_tokens(df):
     for ngram in range(1, 3):
         # min_df -- Removes all words that occur in less than 0.1% of documents
         # max_df -- Removes all words that appear in more than 75% of documents
-        vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), token_pattern=r'\b[a-zA-Z]+\b',
+        vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), token_pattern=r'\b[a-zA-Z]{3,}\b',
                                      min_df=0.001, max_df=0.75, ngram_range=(ngram, ngram))
-        with open(f'abstract_tokens_ngrams_{ngram}.txt', 'w') as abstract_f, \
-                open(f'title_tokens_ngrams_{ngram}.txt', 'w') as title_f:
+        with open(f'../tokens/abstract_tokens_ngrams_{ngram}.txt', 'w') as abstract_f, \
+                open(f'../tokens/title_tokens_ngrams_{ngram}.txt', 'w') as title_f:
             # Write tokens abstract
             vectorizer.fit(corpus_abstract)
             abstract_f.write(repr(vectorizer.get_feature_names()))
@@ -27,7 +27,7 @@ def from_texts_to_tokens(df):
             title_f.write(repr(vectorizer.get_feature_names()))
 
 
-NAME_DF = 'nature_computer_science.csv'
+NAME_DF = '../datasets nature/nature_computer_science.csv'
 df = pd.read_csv(NAME_DF)
 
 if __name__ == "__main__":
